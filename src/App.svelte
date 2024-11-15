280d9f7a56ee049fa09f73116041cb26f77d3eb9
quazlee <quazlee29@gmail.com>
2024-11-15 14:10:03 -0500

    Added Content

--- src/App.svelte
+++ src/App.svelte
@@ -11,27 +11,30 @@
   let year = today.getFullYear();
   let date = today.getDate();
   let clock = () => {
-        today = new Date();
-        hours24 = today.getHours();
-        meridian = hours24 > 12 ? "PM" : "AM";
-        hours12 = Number(today.getHours().toLocaleString()) % 12 || 12;
-        min = today.getMinutes();
-        sec = today.getSeconds();
-        date = today.getDate();
-        month = today.getMonth() + 1;
-        year = today.getFullYear();
+    today = new Date();
+    hours24 = today.getHours();
+    meridian = hours24 > 12 ? "PM" : "AM";
+    hours12 = Number(today.getHours().toLocaleString()) % 12 || 12;
+    min = today.getMinutes();
+    sec = today.getSeconds();
+    date = today.getDate();
+    month = today.getMonth() + 1;
+    year = today.getFullYear();
 
-        setTimeout(clock, 1000);
-    };
-    $: {
-        clock();
-    }
-
+    setTimeout(clock, 1000);
+  };
+  $: {
+    clock();
+  }
 </script>
 
 <main>
   <div id="TopBar">
-    <p>{month}/{date}/{year} {hours24}:{min.toString().padStart(2, "0")} {meridian} 57%</p>
+    <p>
+      {month}/{date}/{year}
+      {hours24}:{min.toString().padStart(2, "0")}
+      {meridian} 90%
+    </p>
   </div>
 
   <div class="mainContainer">
@@ -41,29 +44,53 @@
 
     <div id="RightControls">
       <!-- Zach: I had this here to hopefully set up the display but I just can't figure it out lol-->
-      <h1>placeholder</h1>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d164388.20990939497!2d-84.52020278550863!3d39.13106191862196!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8841b38acb65ec25%3A0x4249b42781c0c5fc!2sUniversity%20of%20Cincinnati!5e1!3m2!1sen!2sus!4v1731696240570!5m2!1sen!2sus"
+        width="800"
+        height="600"
+        style="border:0;"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
     </div>
   </div>
 </main>
 
 <style>
-  *{
+  * {
     margin: 0;
     padding: 0;
   }
 
   #TopBar {
-    position: fixed;
+    /* position: fixed;
     top: 0px;
-    left: 0px;
+    left: 0px; */
+    border: 1px solid black;
+    display: flex;
+    justify-content: flex-start;
   }
 
   .mainContainer {
     display: flex;
     flex-direction: row;
     justify-content: left;
+    border: 1px solid black;
   }
   .mainContainer div {
     justify-content: left;
+  }
+  main {
+    display: flex;
+    flex-direction: column;
+  }
+
+  #LeftControls,
+  #RightControls {
+    border: 1px solid black;
+  }
+  #LeftControls {
+    width: 400px;
   }
 </style>
--- src/lib/QuickControls.svelte
+++ src/lib/QuickControls.svelte
@@ -1,22 +1,95 @@
 <script>
+    import "@fortawesome/fontawesome-free/css/all.min.css";
+
     let frontLeftTirePressure = 32;
     let frontRightTirePressure = 30;
     let backLeftTirePressure = 31;
     let backRightTirePRessure = 32;
+
+    function radioAlert() {
+        window.alert("Clicking this would show the car's radio controls");
+    }
+
+    function gameAlert() {
+        window.alert("Clicking this would show the car's game controls.");
+    }
+
+    function videoAlert() {
+        window.alert("Clicking this would show the car's video controls.");
+    }
+
+    function callingAlert() {
+        window.alert("Clicking this would show the car's calling controls.");
+    }
+
+    function settingsAlert() {
+        window.alert("Clicking this would show the car's settings menu.");
+    }
+
+    function carAlert() {
+        window.alert("Clicking this would show the car overview menu.");
+    }
 </script>
 
+<link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+/>
+
 <div id="AllControls">
+    <div id="NavigationUI">
+        <img
+            id="NavigationImage"
+            src="https://www.tesla.com/ownersmanual/images/GUID-2BE784C9-FDF3-486F-B287-83F7B6BD5FEB-online-en-US.png"
+            alt="navigation picture"
+        />
+    </div>
+
+    <div id="SpotifyControls">
+        <img
+            id="SongCover"
+            src="https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png"
+            alt="album icon"
+        />
+        <div id="SongInformation">
+            <p>Never Gonna Give You Up</p>
+            <p>Rick Astley</p>
+            <p>Whenever You Need Somebody</p>
+            <input type="range" name="" id="" />
+        </div>
+    </div>
     <div id="QuickControls">
         <h3>Quick Controls</h3>
-
+        <div id="QuickControlButtons">
+            <!-- add icons to replace the text for the buttons -->
+            <button on:click={radioAlert}
+                ><i class="fa-solid fa-radio"></i></button
+            >
+            <button on:click={gameAlert}
+                ><i class="fa-solid fa-gamepad"></i></button
+            >
+            <button on:click={videoAlert}><i class="fa-solid fa-tv"></i></button
+            >
+            <button on:click={callingAlert}
+                ><i class="fa-solid fa-phone"></i></button
+            >
+        </div>
     </div>
 
     <div id="CarStats">
         <h3>Tire Pressure (psi):</h3>
-        <p>Front Left:{frontLeftTirePressure} Front Right:{frontRightTirePressure}</p>
-        <p>Back Left:{backLeftTirePressure} Back Right:{backRightTirePRessure}</p>
+        <p>
+            Front Left:{frontLeftTirePressure} Front Right:{frontRightTirePressure}
+        </p>
+        <p>
+            Back Left:{backLeftTirePressure} Back Right:{backRightTirePRessure}
+        </p>
 
         <h3>Odomoter: 27521mi</h3>
+        <button on:click={settingsAlert}
+            ><i class="fa-solid fa-gear"></i></button
+        >
+        <button on:click={carAlert}><i class="fa-solid fa-car"></i></button>
     </div>
 </div>
 
@@ -24,5 +97,28 @@
     #AllControls {
         display: flex;
         flex-direction: column;
+    }
+
+    #QuickControlButtons {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+    }
+
+    p {
+        margin: 0;
+        padding: 0;
+    }
+    #SongCover {
+        width: 25%;
+    }
+    #SpotifyControls {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        border: 1px solid black;
+    }
+    #NavigationImage {
+        width: 25%;
     }
 </style>

a50a9cf119b3341aa285c97ff3c9938ddcc8553f
Jake Huseman <jakeh0916@gmail.com>
2024-11-21 20:03:12 -0500

    styling tweaks

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import QuickControls from "./lib/QuickControls.svelte";
+  import "@fortawesome/fontawesome-free/css/all.min.css";
 
   let today = new Date();
   let hours24 = today.getHours();
@@ -10,11 +11,13 @@
   let month = today.getMonth() + 1;
   let year = today.getFullYear();
   let date = today.getDate();
+  let temp = "46°F"
+  let charge = "86"
   let clock = () => {
     today = new Date();
     hours24 = today.getHours();
     meridian = hours24 > 12 ? "PM" : "AM";
-    hours12 = Number(today.getHours().toLocaleString()) % 12 || 12;
+    hours12 = Number(today.getHours().toLocaleString()) % 12;
     min = today.getMinutes();
     sec = today.getSeconds();
     date = today.getDate();
@@ -28,82 +31,75 @@
   }
 </script>
 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+
 <main>
-  <div class="top">
-    <div id="TopBar">
-      <p>
-        {month}/{date}/{year}
-        {hours24}:{min.toString().padStart(2, "0")}
-        {meridian} 90%
-      </p>
-      <img src="battery.png" height="25" width="25" alt="battery icon" />
+
+  <div id="top-bar">
+    <inl class="top-bar-elem">{hours12}:{min.toString().padStart(2, "0")} {meridian}</inl>
+    <inl class="top-bar-elem">{month}-{date}-{year}</inl>
+    <inl class="top-bar-elem">{temp}</inl>
+    <inl class="top-bar-elem"><i class="fa-solid fa-bolt"></i> {charge}</inl>
+  </div>
+
+  <div class="mainContainer">
+
+    <img alt="maintenance icons" src="maintenance icons.png" width="66" />
+
+    <div id="LeftControls">
+      <QuickControls></QuickControls>
     </div>
 
-    <div class="mainContainer">
-      <div class="maintenance"></div>
-      <img alt="maintenance icons" src="maintenance icons.png" />
-      <div id="LeftControls">
-        <QuickControls></QuickControls>
-      </div>
-
-      <div id="RightControls">
-        <!-- Zach: I had this here to hopefully set up the display but I just can't figure it out lol-->
-        <iframe
-          title="navigation"
-          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d164388.20990939497!2d-84.52020278550863!3d39.13106191862196!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8841b38acb65ec25%3A0x4249b42781c0c5fc!2sUniversity%20of%20Cincinnati!5e1!3m2!1sen!2sus!4v1731696240570!5m2!1sen!2sus"
-          width="800"
-          height="700"
-          style="border:0;"
-          loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
-        ></iframe>
-      </div>
+    <div id="RightControls">
+      <!-- Zach: I had this here to hopefully set up the display but I just can't figure it out lol-->
+      <iframe
+        title="navigation"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d164388.20990939497!2d-84.52020278550863!3d39.13106191862196!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8841b38acb65ec25%3A0x4249b42781c0c5fc!2sUniversity%20of%20Cincinnati!5e1!3m2!1sen!2sus!4v1731696240570!5m2!1sen!2sus"
+        width="800"
+        height="694"
+        style="border-radius: 10px; margin-right: 8px"
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
     </div>
   </div>
+
 </main>
 
 <style>
-  * {
+  main {
     margin: 0;
     padding: 0;
+
+    font-family: 'Segoe UI', Arial, Helvetica, sans-serif;
+
+    border: 6px solid gray;
+    padding: 4px;
+    border-radius: 20px;
   }
 
-  .top {
-    display: flex;
-    flex-direction: column;
+  #top-bar {
     justify-content: left;
-    align-items: left;
+    text-align: left;
+    margin-bottom: 4px;
   }
 
-  #TopBar {
-    /* position: fixed;
-    top: 0px;
-    left: 0px; */
-    border: 1px solid black;
-    display: flex;
-    justify-content: flex-start;
-    gap: 10px;
+  .top-bar-elem {
+    margin: 2px 8px;
+    font-size: 16px;
+    font-weight: bold;
   }
 
   .mainContainer {
     display: flex;
     flex-direction: row;
     justify-content: left;
-    border: 1px solid black;
   }
+
   .mainContainer div {
     justify-content: left;
   }
-  main {
-    display: flex;
-    flex-direction: column;
-  }
 
-  #LeftControls,
-  #RightControls {
-    border: 1px solid black;
-    /* width: 5rem; */
-  }
   #LeftControls {
     width: 300px;
   }
--- src/lib/QuickControls.svelte
+++ src/lib/QuickControls.svelte
@@ -37,96 +37,107 @@
 />
 
 <div id="AllControls">
-    <div id="NavigationUI">
-        <img
-            id="NavigationImage"
-            src="road visualization.jpg"
-            height="200px"
-            width="300px"
-            alt="navigation picture"
-        />
+    <div>
+        <inl class="nav-speed">12</inl><inl>MPH</inl>
+        <inl class="nav-prn">P R N</inl> <inl class="nav-d">D</inl>
+        <img class="nav-and-album-img" src="road_view.png" height="250px" width="250px" alt="navigation" />
     </div>
 
-    <div id="SpotifyControls">
-        <img
-            id="SongCover"
-            src="https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png"
-            height="100"
-            width="100"
-            alt="album icon"
-        />
-        <div id="SongInformation">
-            <p>Never Gonna Give You Up</p>
-            <p>Rick Astley</p>
-            <p>Whenever You Need Somebody</p>
-            <input type="range" name="" id="" />
+    <div id="music-controls">
+        <img src="album.png" height="108" width="108" alt="album" style="display: inline-block" class="nav-and-album-img"/>
+        <div style="display: inline-block; vertical-align: top; padding-top: 4px;">
+            <b>Wonderwall</b>
+            <p>Oasis</p>
+            <input type="range" name="" id="" /><br>
+            <i class="fa-solid fa-play" style="margin: 0 4px;"></i>
+            <i class="fa-solid fa-backward" style="margin: 0 4px;"></i>
+            <i class="fa-solid fa-forward" style="margin: 0 4px;"></i>
+            <i class="fa-brands fa-bluetooth" style="margin: 0 4px;"></i>
         </div>
     </div>
-    <div id="QuickControls">
-        <h3>Quick Controls</h3>
-        <div id="QuickControlButtons">
-            <!-- add icons to replace the text for the buttons -->
-            <button on:click={radioAlert}
-                ><i class="fa-solid fa-radio"></i></button
-            >
-            <button on:click={gameAlert}
-                ><i class="fa-solid fa-gamepad"></i></button
-            >
-            <button on:click={videoAlert}><i class="fa-solid fa-tv"></i></button
-            >
-            <button on:click={callingAlert}
-                ><i class="fa-solid fa-phone"></i></button
-            >
+    <div>
+        <div class="quick-control-row">
+            <button on:click={callingAlert} class="quick-btn"><i class="fa-solid fa-phone"></i></button>
+            <button on:click={settingsAlert} class="quick-btn"><i class="fa-solid fa-gear"></i></button>
+            <button on:click={carAlert} class="quick-btn"><i class="fa-solid fa-car"></i></button>
+        </div>
+        <div class="quick-control-row">
+            <button on:click={radioAlert} class="quick-btn"><i class="fa-solid fa-radio"></i></button>
+            <button on:click={gameAlert} class="quick-btn"><i class="fa-solid fa-gamepad"></i></button>
+            <button on:click={videoAlert} class="quick-btn"><i class="fa-solid fa-tv"></i></button>
         </div>
     </div>
 
-    <div id="CarStats">
-        <h3>Tire Pressure (psi):</h3>
+    <div id="car-stats">
+        <p>Until Next Charge <b>402</b> <inl style="font-size: 12px; color: gray">miles</inl></p>
+        <p>Odomoter (Trip) <b>27521</b> (20) <inl style="font-size: 12px; color: gray">miles</inl></p>
         <p>
-            Front Left:{frontLeftTirePressure} Front Right:{frontRightTirePressure}
+            Front L <b>{frontLeftTirePressure} </b><inl class="units">psi</inl> Front R <b>{frontRightTirePressure} </b><inl class="units">psi</inl>
         </p>
         <p>
-            Back Left:{backLeftTirePressure} Back Right:{backRightTirePRessure}
+            Rear L <b>{backLeftTirePressure} </b><inl class="units">psi</inl> Rear R <b>{backRightTirePRessure} </b><inl class="units">psi</inl>
         </p>
-
-        <h3>Odomoter: 27521mi</h3>
-        <button on:click={settingsAlert}
-            ><i class="fa-solid fa-gear"></i></button
-        >
-        <button on:click={carAlert}><i class="fa-solid fa-car"></i></button>
     </div>
 </div>
 
 <style>
+
+    .nav-speed {
+        font-size: 42px;
+        font-weight: bolder;
+    }
+
+    .nav-prn {
+        color: gray;
+        margin-left: 40px;
+        font-size: 18px;
+    }
+
+    .nav-d {
+        font-weight: bolder;
+        font-size: 18px;
+    }
+
+    .nav-and-album-img {
+        border-radius: 12px;
+    }
+
+    #music-controls {
+        min-height: 108px;
+        margin-top: 12px;
+    }
+
     #AllControls {
         display: flex;
         flex-direction: column;
     }
 
-    #QuickControlButtons {
+    .quick-control-row {
         display: flex;
         flex-direction: row;
         justify-content: center;
+        align-content: center;
+        margin-top: 8px;
+    }
+
+    .quick-btn {
+        min-width: 72px;
+        min-height: 56px;
+        margin: 4px 8px;
+        font-size: 18px
+    }
+
+    .units {
+        font-size: 12px;
+        color: gray;
+    }
+
+    #car-stats {
+        margin-top: 10px;
     }
 
     p {
         margin: 0;
         padding: 0;
-    }
-    #SpotifyControls {
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        border: 1px solid black;
-    }
-    #SongCover {
-        display: flex;
-        margin-left: 5px;
-        margin-top: 10px;
-    }
-    #NavigationImage {
-        display: flex;
-        justify-content: left;
-        align-items: left;
     }
 </style>
